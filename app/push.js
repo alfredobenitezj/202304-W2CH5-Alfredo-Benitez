7a94e3f8ed67b1b17eb452c2958660e2e3098042
Alfredo <alfredobenitezjuanilla@gmail.com>
2023-05-07 16:10:11 +0200

    Add push error

--- app/push.js
+++ app/push.js
@@ -2,6 +2,6 @@ const alumn = ["alejandro", "Coba"];
 const addWord = (Array, ...addelement) => {
   let AddAlumn = [..."Joseba"];
   for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+    array[array.length] = AddAlumn[index];
   }
 };
